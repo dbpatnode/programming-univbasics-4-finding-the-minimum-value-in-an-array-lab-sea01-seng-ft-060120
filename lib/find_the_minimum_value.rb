af40cc5317f921686596ccd6792c31065c009272
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-11 19:45:04 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,8 +1,9 @@
 def find_min_value(array)
   count = 0 
+  min_value = 0
   while count < array.length do
-   if Math.max(array) < array[count]
-     Math.max(array) = array[count]
+   if min_value < array[count]
+     min_value = array[count]
    end
    count += 1
   end
